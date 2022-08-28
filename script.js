3ec78cb7d3b8098d0c757c7167e96340fb92fe0b
Victoria <adedayovicky123@gmail.com>
2022-08-28 11:51:26 +0100

    mobile menu

---
+++ script.js
@@ -0,0 +1,16 @@
+const menuClose = document.getElementById("closeMenuBtn");
+const menuOpen = document.getElementById("openMenuBtn");
+const mobileMenu = document.querySelector(".mobile-nav");
+
+console.log(menuClose, menuOpen, mobileMenu);
+
+menuOpen.addEventListener("click", () => {
+  mobileMenu.classList.toggle("show");
+  // menuClose.style.display = "block";
+});
+
+menuClose.addEventListener("click", () => {
+  mobileMenu.classList.toggle("show");
+  // mobileMenu.classList.contains('show')
+  // menuClose.style.display = "none";
+});
