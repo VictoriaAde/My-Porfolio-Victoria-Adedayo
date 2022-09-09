8d3dfa998dd3e21e751234e50ad7c4442a291773
Maverick <supacode@gmail.com>
2022-09-09 05:22:15 +0100

    Scroll reveal about section

--- src/js/scrollReaveal.js
+++ src/js/scrollReaveal.js
@@ -1,22 +1,42 @@
 import ScrollReveal from 'scrollreveal';
 
-const els = [
-  document.querySelector('.hero__welcome'),
-  document.querySelector('.hero__heading'),
-  document.querySelector('.hero__subheading'),
-  document.querySelector('.hero__description'),
-  document.querySelector('.hero__cta'),
-  document.querySelector('.hero__img'),
+const scrollRevealOptions = {
+  duration: 500,
+  origin: 'bottom',
+  distance: '20px',
+  easing: 'ease-in',
+  opacity: 0,
+  reset: false,
+};
+
+const heroSelectors = [
+  // Hero
+  { el: '.hero__welcome' },
+  { el: '.hero__heading' },
+  { el: '.hero__subheading' },
+  { el: '.hero__description' },
+  { el: '.hero__cta' },
+  { el: '.hero__img' },
+  // About
+  { el: '.about__heading' },
+  { el: '.about__text--1' },
+  { el: '.about__text--2' },
+  { el: '.about__text--3' },
+  { el: '.about__text--4' },
+  { el: '.about__text--5' },
+  { el: '.about__img', options: { origin: 'right' } },
+
   //  TODO: Add more selectors to scroll reveal here
 ];
 
+const selectors = [...heroSelectors];
+
 export const runScrollReveal = () => {
-  ScrollReveal().reveal(els, {
-    duration: 500,
-    origin: 'bottom',
-    distance: '20px',
-    easing: 'ease-in',
-    opacity: 0,
-    reset: true,
+  heroSelectors.forEach((el, index) => {
+    ScrollReveal().reveal(el.el, {
+      ...scrollRevealOptions,
+      ...{ delay: index * 100 },
+      ...el.options,
+    });
   });
 };
