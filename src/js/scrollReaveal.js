c9e83dbc021ea1cd7825b5d64ac066a134bfb3df
Victoria <adedayovicky123@gmail.com>
2022-09-14 00:09:06 +0100

    add scroll effect to other sections
    adjust style and spacing

--- src/js/scrollReaveal.js
+++ src/js/scrollReaveal.js
@@ -25,7 +25,30 @@ const heroSelectors = [
   { el: '.about__text--4' },
   { el: '.about__text--5' },
   { el: '.about__img', options: { origin: 'right' } },
-
+  //project
+  { el: '.project_con__heading' },
+  { el: '.project_con__text' },
+  { el: '.project_con__projects' },
+  { el: '.project_con__github_link' },
+  { el: '.project_con__triangle_shape--mobile' },
+  { el: '.project_con__triangle_shape--desktop' },
+  //blog
+  { el: '.blog__heading' },
+  { el: '.blog__item_img_one' },
+  { el: '.blog__item_description' },
+  { el: '.blog__item_img_two' },
+  { el: '.blog__item_img_three' },
+  //contact
+  { el: '.contact__heading' },
+  { el: '.contact__text' },
+  { el: '.contact__uptriangle_shape_mobile' },
+  { el: '.contact__uptriangle_shape_desktop' },
+  { el: 'input' },
+  { el: 'textarea' },
+  //footer
+  { el: '.footer__logo_div' },
+  { el: '.footer__links_div' },
+  { el: '.footer__copyright_div' },
   //  TODO: Add more selectors to scroll reveal here
 ];
 
@@ -35,7 +58,7 @@ export const runScrollReveal = () => {
   heroSelectors.forEach((el, index) => {
     ScrollReveal().reveal(el.el, {
       ...scrollRevealOptions,
-      ...{ delay: index * 100 },
+      ...{ delay: index * 20 },
       ...el.options,
     });
   });
